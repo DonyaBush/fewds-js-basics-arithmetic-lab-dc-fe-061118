6da22b81013b3b517d39c319dc5d528a9084070f
Ki Ki  Bush <donyabush@gmail.com>
2018-07-25 00:35:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ var age = 'currentAge';
 
 function currentAge() {
   var res = 'ageIsValid'
-   
+   res = res + Number.isInteger(123) + ": 123";
+   res = res + Number.isInteger(0.0) + ": currentAge"
      document.getElementById("currentAge").innerHTML = res;
 }
