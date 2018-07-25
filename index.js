f12361aa5a432499b559ebb226ac1bdd5eb2392b
Ki Ki  Bush <donyabush@gmail.com>
2018-07-25 00:20:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 
 
- newID = 1000000001 + oldID
+ newID = 1000000000 + oldID
 //console.log(oldID)
 //console.log(currentAge)
 var age = 'currentAge';
